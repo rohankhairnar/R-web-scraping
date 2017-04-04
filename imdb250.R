1cae5808eedb1f16c2d60b24a5e002d5b2539b76
temp <temp@Rohans-MacBook-Pro.local>
2017-04-04 16:59:26 -0400

    rm commented lines

--- imdb250.R
+++ imdb250.R
@@ -11,9 +11,6 @@ imdb_250 %>%
   html_nodes(".secondaryInfo , strong , #main a") %>%
   html_text()
 
-
-#write_html(imdb_250, file = "/Users/temp/Desktop/R practise/imdbhtml.txt")
-
 movie_list <- imdb_250 %>%
   html_nodes(".titleColumn a") %>%
   html_text()
@@ -29,9 +26,6 @@ movie_rating<-imdb_250 %>%
 
 all_nodes<-html_nodes(imdb_250, ".titleColumn a")
 
-#write_html(all_nodes,file = "/Users/temp/Desktop/R practise/nodes.html")
-
-#html_attrs(all_nodes)
 
 key_cast<- sapply(html_attrs(all_nodes),'[[','title')
 movie_link <- paste0(base_url,sapply(html_attrs(all_nodes),'[[','href'))
@@ -41,10 +35,8 @@ movie_link <- paste0(base_url,sapply(html_attrs(all_nodes),'[[','href'))
 rank <- c(1:length(movie_link))
 
 x<- html_nodes(imdb_250, ".ratingColumn strong")
-#gsub("//D","",x)%>% 
 voters_count<- gsub(".*?based on ","",gsub(" user ratings.*","",x))
 
 db_250 = data.frame(rank, movie_list, key_cast, movie_year, movie_rating, voters_count, movie_link)
-
 write.csv(db_250, file = "/Users/temp/Desktop/R practise/imdb250.csv")
 
